58d453a34a4dbc18e955217393b5b8d8e5d31b01
Tirth Patel <59409218+pappu6@users.noreply.github.com>
2021-04-24 15:45:42 +0530

    Payment functionality done in functions, Carousel added, footer added

---
+++ App.js
@@ -0,0 +1,78 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from './Checkout';
+import Login from './Login';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+import Payment from './Payment';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import Orders from './Orders';
+import Footer from './Footer';
+
+const promise = loadStripe("pk_test_51HQsurEAhsHjBflQECxSOhDtFuACfiluFkagwDMrDtGqpWTuKWYXpQy1MFxi5pJ63GJx8uSDMtfZOOzV67o09PNL00lJmJShUX");
+
+function App() {
+  const [{ }, dispatch] = useStateValue();
+
+  useEffect(() => {
+
+    auth.onAuthStateChanged(authUser => {
+
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
+  return (
+    <Router>
+      <div className="app">
+
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+            <Footer />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+            <Footer />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+            <Footer />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+            <Footer />
+          </Route>
+
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
